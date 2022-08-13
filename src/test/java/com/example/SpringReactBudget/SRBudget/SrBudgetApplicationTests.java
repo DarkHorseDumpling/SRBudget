2791d123125c859253e1031f9cf6d8b7e341c661
DarkHorseDumpling <54558101+DarkHorseDumpling@users.noreply.github.com>
2022-08-13 14:41:55 -0400

    Initial git for srbudget

---
+++ src/test/java/com/example/SpringReactBudget/SRBudget/SrBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringReactBudget.SRBudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
